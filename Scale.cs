809420c8977f4337cb778ec00f178ae3760e7e89
David <dsaffo@luc.edu>
2016-05-19 20:57:06 -0500

    hi
    hi

---
+++ Scale.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scale : MonoBehaviour {
+
+    private bool isActive = false;
+
+    CardboardHead head = null;
+
+	// Use this for initialization
+	void Start () {
+
+        head = Camera.main.GetComponent<StereoController>().Head;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isActive && Cardboard.SDK.Triggered)
+        {
+            isActive = true;
+        }
+
+        else if (isActive && Cardboard.SDK.Triggered)
+        {
+            isActive = false;
+        }
+
+        if (isActive)
+        {
+            transform.localScale += new Vector3(0.1f, 0.1f, 0.1f).normalized * 1 * Time.deltaTime;
+        }
+	
+	}
+}
